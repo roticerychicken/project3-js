15ead004e6d0e6f0abdd082c7cd3c09dc9e88327
Jonah Patinkin <jonahpatinkin@gmail.com>
2016-12-06 23:23:02 -0600

    added calender for selecting dates

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
-  var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2016-11-20&end_date=2016-11-27&api_key=8JKWyLMEukb15NV4yy658pIo59dMOpePKRcNywOx";
+  
+
+ 
+
+$( function() {
+    var dateFormat = "mm/dd/yy",
+      from = $( "#from" )
+        .datepicker({
+          defaultDate: "+1w",
+          changeMonth: true,
+          numberOfMonths: 3
+        })
+        .on( "change", function() {
+          to.datepicker( "option", "minDate", getDate( this ) );
+        }),
+      to = $( "#to" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3
+      })
+      .on( "change", function() {
+        from.datepicker( "option", "maxDate", getDate( this ) );
+      });
+ 
+    function getDate( element ) {
+      var date;
+      try {
+        date = $.datepicker.parseDate( dateFormat, element.value );
+      } catch( error ) {
+        date = null;
+      }
+ 
+      return date;
+    }
+});
+
+
+ var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2016-11-20&end_date=2016-11-27&api_key=8JKWyLMEukb15NV4yy658pIo59dMOpePKRcNywOx";
   $(document).ready(function() {
     console.log("on clicked");
     $.getJSON(url,function(json){
@@ -13,7 +50,7 @@
           //.each is a loop
           $.each(objs, function(i, obj) {
             //string append all that text in the parentheses to the string and then at the end of the loop we will put the enire string in a div problem solved biatch
-            strObj += "<li> astroid name: " + obj.name + "</li>";
+            strObj += "<li> Astroid Name: " + obj.name + "</li>";
           });  
           $("#main").append("<div class=post>" + strObj + "</div>");
           strObj = "";
