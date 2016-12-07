0e5dc23c6e1b902e626542e1c2208bcf355576ea
Jonah Patinkin <jonahpatinkin@gmail.com>
2016-12-07 12:49:41 -0600

    added user input

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-  
-
- 
-
-$( function() {
-    var dateFormat = "mm/dd/yy",
+/*
+ $( function() {
+    var dateFormat = "yy/dd/yy",
       from = $( "#from" )
         .datepicker({
           defaultDate: "+1w",
           changeMonth: true,
-          numberOfMonths: 3
+          numberOfMonths: 1
         })
         .on( "change", function() {
           to.datepicker( "option", "minDate", getDate( this ) );
@@ -16,12 +13,12 @@ $( function() {
       to = $( "#to" ).datepicker({
         defaultDate: "+1w",
         changeMonth: true,
-        numberOfMonths: 3
+        numberOfMonths: 1
       })
       .on( "change", function() {
         from.datepicker( "option", "maxDate", getDate( this ) );
       });
- 
+
     function getDate( element ) {
       var date;
       try {
@@ -32,28 +29,45 @@ $( function() {
  
       return date;
     }
-});
+  });
+*/
+  $(document).ready(function () { 
 
 
- var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2016-11-20&end_date=2016-11-27&api_key=8JKWyLMEukb15NV4yy658pIo59dMOpePKRcNywOx";
-  $(document).ready(function() {
-    console.log("on clicked");
-    $.getJSON(url,function(json){
-      console.log("got json");
+    
 
-      var strObj = "";
 
-      //near eath objects is a dictionary   and it also has a function containing a date and an objecy
-      $.each(json.near_earth_objects, function(date, objs){
+      $("#loadData").click(function() {
+
+
+
         
-        strObj += "<li class=date><b>"+date+"</b></li>";
-          //.each is a loop
-          $.each(objs, function(i, obj) {
-            //string append all that text in the parentheses to the string and then at the end of the loop we will put the enire string in a div problem solved biatch
-            strObj += "<li> Astroid Name: " + obj.name + "</li>";
-          });  
-          $("#main").append("<div class=post>" + strObj + "</div>");
-          strObj = "";
-      });
-   });
-});
+
+
+        var date1 = document.getElementById("from").value;
+        var date2 = document.getElementById("to").value;
+
+        var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date="+date1+"&end_date="+date2+"&api_key=8JKWyLMEukb15NV4yy658pIo59dMOpePKRcNywOx";
+
+        
+      var strObj = "";
+      console.log("on clicked");
+      $.getJSON(url,function(json){
+        console.log("got json");
+
+      
+        //near eath objects is a dictionary   and it also has a function containing a date and an objecy
+        $.each(json.near_earth_objects, function(date, objs){
+          
+          strObj += "<li class=date><b>"+date+"</b></li>";
+            //.each is a loop
+            $.each(objs, function(i, obj) {
+              //string append all that text in the parentheses to the string and then at the end of the loop we will put the enire string in a div problem solved biatch
+              strObj += "<li> Astroid Name: " + obj.name + "</li>";
+            });  
+            $("#main").append("<div class=post>" + strObj + "</div>");
+            strObj = "";
+          });
+       });
+    });
+  });
